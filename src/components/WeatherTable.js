4f8b437630850f151f6ca3c6220823f9930d5069
gyss <gravedian@gmail.com>
2020-07-22 22:34:26 +0100

    added table responsiveness

--- src/components/WeatherTable.js
+++ src/components/WeatherTable.js
@@ -4,20 +4,28 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {css, jsx} from '@emotion/core'
 
-// import {SortableContainer, SortableElement} from 'react-sortable-hoc'
+import {SortableContainer, SortableElement} from 'react-sortable-hoc'
 import {defaultTableRowRenderer, AutoSizer, Table, Column} from 'react-virtualized'
 
 import {fetchWeather} from '../actions'
 
 const container = css`
   height: 100%;
+  width: 100%;
+  overflow: hidden;
 `
 
-const headerStyles = css`
-  padding: 2em;
+const header = css`
+  padding: calc(2em + 10px);
   background-color: #fafbfc;
   margin-bottom: 2em;
   box-shadow: inset 0 -1px 0 #e1e4e8;
+`
+
+const table = css`
+  overflow: auto;
+  padding: 0 2em;
+  height: 100%;
 `
 
 /*
@@ -38,8 +46,6 @@ const WeatherTable = ({dispatch, loading, error, city, list}) => {
     dispatch(fetchWeather())
   }, [])
 
-  console.log(loading, error, city, list)
-
   if (loading) {
     return <div>Loading weather data...</div>
   }
@@ -48,90 +54,107 @@ const WeatherTable = ({dispatch, loading, error, city, list}) => {
     return <div>ERROR!</div>
   }
 
-  list.forEach((element) => {
-    //if (element.rain) {
-    //console.log('>>>', element.weather.length)
-    //}
-  })
-
   return (
     <div css={container}>
-      <div css={headerStyles}>
+      <div css={header}>
         <h2>
           {city.name}, {city.country}
         </h2>
       </div>
 
-      <AutoSizer>
-        {({height, width}) => (
-          <Table
-            width={width}
-            height={height}
-            headerHeight={40}
-            rowHeight={30}
-            rowCount={list.length}
-            rowGetter={({index}) => list[index]}>
-            <Column label="Date" dataKey="dt_txt" width={350} />
-            <Column width={300} label="Visibility" dataKey="visibility" />
-            <Column
-              width={300}
-              label="Wind"
-              dataKey="wind"
-              cellDataGetter={({rowData}) => rowData.wind.speed + ' / ' + rowData.wind.deg}
-            />
-            <Column width={300} label="Clouds" dataKey="clouds" cellDataGetter={({rowData}) => rowData.clouds.all} />
-            <Column
-              width={300}
-              label="Rain"
-              dataKey="rain"
-              cellDataGetter={({rowData}) => rowData.rain && rowData.rain['3h']}
-            />
-            <Column
-              width={300}
-              label="Feels like"
-              dataKey="feels_like"
-              cellDataGetter={({rowData}) => rowData.main.feels_like}
-            />
-            <Column
-              width={300}
-              label="Grnd Level"
-              dataKey="grnd_level"
-              cellDataGetter={({rowData}) => rowData.main.grnd_level}
-            />
-            <Column
-              width={300}
-              label="Humidity"
-              dataKey="humidity"
-              cellDataGetter={({rowData}) => rowData.main.humidity}
-            />
-            <Column
-              width={300}
-              label="Pressure"
-              dataKey="pressure"
-              cellDataGetter={({rowData}) => rowData.main.pressure}
-            />
-            <Column
-              width={300}
-              label="Sea Level"
-              dataKey="sea_level"
-              cellDataGetter={({rowData}) => rowData.main.sea_level}
-            />
-            <Column width={300} label="Temp" dataKey="temp" cellDataGetter={({rowData}) => rowData.main.temp} />
-            <Column
-              width={300}
-              label="Temp min"
-              dataKey="temp_min"
-              cellDataGetter={({rowData}) => rowData.main.temp_min}
-            />
-            <Column
-              width={300}
-              label="Temp max"
-              dataKey="temp_max"
-              cellDataGetter={({rowData}) => rowData.main.temp_max}
-            />
-          </Table>
-        )}
-      </AutoSizer>
+      <div css={table}>
+        <AutoSizer>
+          {({height, width}) => (
+            <Table
+              width={Math.max(1570, width)}
+              height={height}
+              headerHeight={40}
+              rowHeight={30}
+              rowCount={list.length}
+              rowGetter={({index}) => list[index]}>
+              <Column label="Date" dataKey="dt_txt" width={200} flexGrow={1} />
+              <Column width={110} label="Visibility" dataKey="visibility" flexGrow={1} />
+              <Column
+                width={100}
+                label="Wind"
+                dataKey="wind"
+                cellDataGetter={({rowData}) => rowData.wind.speed + ' / ' + rowData.wind.deg}
+                flexGrow={1}
+              />
+              <Column
+                width={100}
+                label="Clouds"
+                dataKey="clouds"
+                cellDataGetter={({rowData}) => rowData.clouds.all}
+                flexGrow={1}
+              />
+              <Column
+                width={100}
+                label="Rain"
+                dataKey="rain"
+                cellDataGetter={({rowData}) => rowData.rain && rowData.rain['3h']}
+                flexGrow={1}
+              />
+              <Column
+                width={120}
+                label="Feels like"
+                dataKey="feels_like"
+                cellDataGetter={({rowData}) => rowData.main.feels_like}
+                flexGrow={1}
+              />
+              <Column
+                width={140}
+                label="Grnd Level"
+                dataKey="grnd_level"
+                cellDataGetter={({rowData}) => rowData.main.grnd_level}
+                flexGrow={1}
+              />
+              <Column
+                width={120}
+                label="Humidity"
+                dataKey="humidity"
+                cellDataGetter={({rowData}) => rowData.main.humidity}
+                flexGrow={1}
+              />
+              <Column
+                width={120}
+                label="Pressure"
+                dataKey="pressure"
+                cellDataGetter={({rowData}) => rowData.main.pressure}
+                flexGrow={1}
+              />
+              <Column
+                width={120}
+                label="Sea Level"
+                dataKey="sea_level"
+                cellDataGetter={({rowData}) => rowData.main.sea_level}
+                flexGrow={1}
+              />
+              <Column
+                width={120}
+                label="Temp"
+                dataKey="temp"
+                cellDataGetter={({rowData}) => rowData.main.temp}
+                flexGrow={1}
+              />
+              <Column
+                width={120}
+                label="Temp min"
+                dataKey="temp_min"
+                cellDataGetter={({rowData}) => rowData.main.temp_min}
+                flexGrow={1}
+              />
+              <Column
+                width={120}
+                label="Temp max"
+                dataKey="temp_max"
+                cellDataGetter={({rowData}) => rowData.main.temp_max}
+                flexGrow={1}
+              />
+            </Table>
+          )}
+        </AutoSizer>
+      </div>
 
       {/*<List className="List" height={300} itemCount={list.length} itemSize={35}>
         {({index, style}) => {
